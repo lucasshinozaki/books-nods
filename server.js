3fe90a27accd196efed805cbf594f5143cb7992e
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-22 12:44:11 -0300

    Iniciando projeto

---
+++ server.js
@@ -0,0 +1,19 @@
+import http from "http"
+
+const PORT = 3000
+
+const rotas = {
+    "/": "Curso de Express API",
+    "/livros" : "Entrei na rota livros",
+    "/autores" : "Entrei na rota autrores"
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"})
+    res.end(rotas[req.url])
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta: ${PORT}`)
+})
