6846b215df39c8769edf4dae5bef9804a1ba4d55
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-24 18:34:23 -0300

    Validação personalizada

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,7 +6,15 @@ const livroSchema = new mongoose.Schema({
         type: String, 
         required: [true, "O titulo do livro é obrigatório"]
     },
-    preco: {type: Number},
+    preco: {
+        type: Number,
+        validate: {
+            validator: (valor) => {
+                return valor >= 10 && valor <= 5000
+            },
+            message: "O valor deve estar entre 10 a 5000. Valor fornecido {VALUE}"
+        }
+    },
     paginas: {
         type: Number,
         min: [10, "O numero de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}"],
